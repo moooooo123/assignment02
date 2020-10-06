ba0766e34fa7f8c572749b116277d2ebf9a2f22e
Owner <Owner@DESKTOP-HPM6MEK.lan>
2020-10-05 19:49:40 -0700

    committing files

---
+++ cse360assignment2/AddingMachine.java
@@ -0,0 +1,26 @@
+package cse360assignment02;
+
+public class AddingMachine {
+  private int total;
+  
+  public AddingMachine () {
+    total = 0;  // not needed - included for clarity
+  }
+  //returns total
+  public int getTotal () {
+    return 0;
+  }
+  //adds value to total
+  public void add (int value) {
+  }
+//subtracts value from total
+  public void subtract (int value) {
+  }
+//records the previous transactions
+  public String toString () {
+    return "";
+  }
+//clears toString
+  public void clear() {
+  }
+}
