203ab565627ecf877b4abfc46ff13cfd7f5f9fb3
Owner <Owner@DESKTOP-HPM6MEK.lan>
2020-10-05 23:35:44 -0700

    commit message 2

--- cse360assignment2/AddingMachine.java
+++ cse360assignment2/AddingMachine.java
@@ -2,25 +2,37 @@ package cse360assignment02;
 
 public class AddingMachine {
   private int total;
+  private String history;
   
   public AddingMachine () {
     total = 0;  // not needed - included for clarity
   }
   //returns total
   public int getTotal () {
-    return 0;
+	  
+    return total;
   }
   //adds value to total
   public void add (int value) {
+	  total +=value;
+	  addString("+", value);
   }
 //subtracts value from total
   public void subtract (int value) {
+	  total -=value;
+	  addString("-", value);
   }
-//records the previous transactions
+  public String addString(String op, int val)
+  {
+	  String intString=Integer.toString(val);
+	  history +=intString+" "+op+" ";
+  }
+//returns history of transactions
   public String toString () {
-    return "";
+    return history;
   }
 //clears toString
   public void clear() {
+	  history="";
   }
 }
